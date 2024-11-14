4997df85299dbbe6b03452c746bd01b61e46cfe1
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2024-11-14 14:23:14 +0800

    feat: create ui ux for Auth Page

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <slot name="header" />
+        </div>
+        <div class="card-body">
+            <slot name="body" />
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 1rem;
+    background-color: #fff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+    font-weight: bold;
+    font-size: 1.25rem;
+    padding-bottom: 0.5rem;
+    border-bottom: 1px solid #eee;
+}
+
+.card-body {
+    padding: 1rem 0;
+    font-size: 1rem;
+    color: #333;
+    margin-bottom: 1rem;
+}
+</style>
--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,23 +1,34 @@
 <template>
-    <form @submit.prevent="login">
-        <div>
-            <label for="">Username</label>
-            <input type="text" v-model="formData.username">
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input type="text" v-model="formData.password">
-        </div>
-        <div>
-            <button type="submit">Login</button>
-        </div>
-        <pre>{{ formData }}</pre>
-    </form>
+
+    <div class="fluid">
+        <Card class="auth-card">
+            <template #header>
+                <div class="header-content">
+                    <h3>Archintel Articles</h3>
+                    <span>Created by: Joenell Alonzo</span>
+                </div>
+            </template>
+            <template #body>
+                <form @submit.prevent="login">
+                    <FormInput v-model="formData.username" label="Username" id="a-username" type="text"
+                        placeholder="" />
+                    <FormInput v-model="formData.password" label="Password" id="a-pasword" type="password"
+                        placeholder="" />
+                    <div>
+                        <Button class="auth-btn" type="submit">Login</Button>
+                    </div>
+                </form>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router';
 import { ref } from 'vue'
+import Card from '../components/UI/Card.vue';
+import FormInput from '../components/UI/FormInput.vue';
+import Button from '../components/UI/Button.vue';
 import api from '../api';
 const formData = ref({
     username: '',
@@ -61,4 +72,63 @@ const login = async () => {
 
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.fluid {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+h2 {
+    font-size: 5rem;
+    color: var(--text-lighter);
+}
+
+h3 {
+    text-align: center;
+    font-size: 3rem;
+    color: var(--text-lighter);
+    margin-bottom: 2rem;
+}
+
+.auth-btn {
+    width: 100%;
+}
+
+.auth-card {
+    width: 60vh;
+}
+
+.header-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.header-content {
+    margin-top: 1rem;
+}
+
+.header-content h3 {
+    color: #000;
+    font-size: 3rem;
+    font-weight: 500;
+    margin-bottom: 0;
+}
+
+.header-content span {
+    color: var(--text-lighter);
+    font-size: 1.3rem;
+    margin-bottom: 1.5rem;
+}
+
+@media screen and (max-width: 768px) {
+    .auth-card {
+        width: 90vh;
+    }
+}
+</style>
